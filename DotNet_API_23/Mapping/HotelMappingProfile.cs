ef26b3c76b6e8070c4346ee6d47bba82dcb4cdea
tapan0810 <arceus0810@gmail.com>
2026-03-10 22:28:21 +0530

    feat : Implemented AutoMapper

---
+++ DotNet_API_23/Mapping/HotelMappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using DotNet_API_23.Entities.Dtos;
+using DotNet_API_23.Entities.Models;
+
+namespace DotNet_API_23.Mapping
+{
+    public class HotelMappingProfile : Profile
+    {
+       public HotelMappingProfile() {
+
+            CreateMap<Hotel, GetAllHotelsDto>();
+
+            CreateMap<Hotel, GetHotelyIdDto>();
+
+            CreateMap<CreateHotelDto, Hotel>();
+
+            CreateMap<UpdateHotelDto, Hotel>();
+        }
+    }
+}
