520596fd2ac660ab3a3242da0cdcc738b2818d8a
tapan0810 <arceus0810@gmail.com>
2026-03-10 22:16:14 +0530

    feat: Implemented Hotel Model and Dtos

---
+++ DotNet_API_23/Entities/Dtos/CreateHotelDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DotNet_API_23.Entities.Dtos
+{
+    public class CreateHotelDto
+    {
+        public string HotelName { get; set; } = string.Empty;
+        public string HotelAddress { get; set; } = string.Empty;
+        public string HotelCity { get; set; } = string.Empty;
+        [Phone]
+        public string Phone { get; set; } = string.Empty;
+    }
+}
---
+++ DotNet_API_23/Entities/Models/Hotel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DotNet_API_23.Entities.Models
+{
+    public class Hotel
+    {
+        public int Id { get; set; }
+        public string HotelName { get; set; } = string.Empty;
+        public string HotelAddress { get; set; } = string.Empty;
+        public string HotelCity { get; set;} = string.Empty;
+        [Phone]
+        public string Phone { get; set; } = string.Empty;
+    }
+}
